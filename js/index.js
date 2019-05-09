6834b95c325d0085beab1be23e771b715ca4d26f
LilyMart7 <lilymart7@gmail.com>
2019-05-09 20:29:23 +0300

    first commit

---
+++ js/index.js
@@ -0,0 +1,18 @@
+
+let xhr = new XMLHttpRequest();
+
+xhr.open("GET", "https://swapi.co/api/planets/1/", true);
+xhr.send();
+
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState != 4) return;
+
+        if (xhr.status != 200) {
+            var errStatus = xhr.status;
+            var errText = xhr.statusText;
+            console.log(errStatus + ": " + errText);
+        } else {
+            var data = JSON.parse(xhr.responseText);
+              console.log(data);      
+        }
+    };
